35d70f494c8358838ce95df65c69e25aeb065b1a
Luisa Ramirez <luisa.ramirez3@utp.edu.co>
2024-05-25 19:54:10 -0500

    Login view

--- Server/src/main/java/com/example/server/repository/IPersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.repository;
-
-import com.example.server.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPersonRepository extends JpaRepository<Person, Long> {
-}
