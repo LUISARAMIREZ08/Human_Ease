d4f73ea16d11fd157f9ab2d1a43a4281b166eb77
Luisa Ramirez <luisa.ramirez3@utp.edu.co>
2024-05-29 16:31:51 -0500

    Add employee-table

--- Client/src/app/components/trabajadores/trabajadores.component.ts
+++ Client/src/app/components/trabajadores/trabajadores.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import HeaderComponent from '../header/header.component';
 import BarraHerramientasComponent from '../barra-herramientas/barra-herramientas.component';
+import { ApiService } from '../../services/api.service';
 
 export interface Trabajador {
   id: number;
@@ -14,21 +15,6 @@ export interface Trabajador {
   phone: string;
 }
 
-const ELEMENT_DATA: Trabajador[] = [
-  {id: 16273672, name: 'Juan', last_name: 'Perez', role_person: 'Cocinero', salary_base: 1000, phone: '123456789'},
-  {id: 227366351133, name: 'Pedro', last_name: 'Ramirez', role_person: 'Mesero', salary_base: 800, phone: '123456789'},
-  {id: 327366351133, name: 'Maria', last_name: 'Gonzalez', role_person: 'Cajero', salary_base: 900, phone: '123456789'},
-  {id: 427366351133, name: 'Ana', last_name: 'Martinez', role_person: 'Cocinero', salary_base: 1000, phone: '123456789'},
-  {id: 527366351133, name: 'Jose', last_name: 'Lopez', role_person: 'Mesero', salary_base: 800, phone: '123456789'},
-  {id: 627366351133, name: 'Luis', last_name: 'Rodriguez', role_person: 'Cajero', salary_base: 900, phone: '123456789'},
-  {id: 727366351133, name: 'Carlos', last_name: 'Hernandez', role_person: 'Cocinero', salary_base: 1000, phone: '123456789'},
-  {id: 827366351133, name: 'Rosa', last_name: 'Gutierrez', role_person: 'Mesero', salary_base: 800, phone: '123456789'},
-  {id: 927366351133, name: 'Sofia', last_name: 'Diaz', role_person: 'Cajero', salary_base: 900, phone: '123456789'},
-  {id: 1027366351133, name: 'Fernando', last_name: 'Sanchez', role_person: 'Cocinero', salary_base: 1000, phone: '123456789'},
-  {id: 1127366351133, name: 'Luisa', last_name: 'Perez', role_person: 'Mesero', salary_base: 800, phone: '123456789'},
-  {id: 1273663511332, name: 'Jorge', last_name: 'Ramirez', role_person: 'Cajero', salary_base: 900, phone: '123456789'},
-];
-
 @Component({
   selector: 'app-trabajadores',
   standalone: true,
@@ -36,10 +22,21 @@ const ELEMENT_DATA: Trabajador[] = [
   templateUrl: './trabajadores.component.html',
   styleUrl: './trabajadores.component.css'
 })
-export default class TrabajadoresComponent {
+export default class TrabajadoresComponent implements OnInit{
+  constructor(private api: ApiService) { }
   displayedColumns: string[] = ['id', 'name', 'last_name', 'role_person', 'salary_base', 'phone'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  data:any;
+  dataSource:any;
   clickedRows = new Set<Trabajador>();
+
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') { // Esto fue añadido por localstorage indefinidio en el navegador
+      this.api.getAPI('employee/all').subscribe((data: Trabajador[]) => {
+        this.dataSource = new MatTableDataSource(data);
+      });
+      this.dataSource = new MatTableDataSource(this.data);
+    }
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
